a26406a6d7c0519a81da49726ade63bbf2585d43
Nuroferatu <32645518+Nuroferatu@users.noreply.github.com>
2020-05-19 15:39:51 +0200

    Read header and set source/destination language based on header content

--- lib/translationfile.php
+++ lib/translationfile.php
@@ -3,21 +3,50 @@
 class TranslationFile {
     private $handle;
     private $name;
-    
+    private $srcLang;
+    private $destLang;
+
+    // FatalError means... this class object is usles, we may as well terminate application
+    private function fatalError( $message ) {
+        throw new RuntimeException( $message . PHP_EOL );
+    }
+
+    // Simlle switch to validate lang code
+    private function isValidLangCode($langCode) {
+        switch( $langCode ) {
+            case "en":
+            case "pl":
+                return true;
+        }
+        return false;
+    }
+
+    // First line (header) must contain language pair in format: srcLang;destLang
+    private function processHeader() {
+        $firstLine = fgets( $this->handle );
+        $firstLine = str_replace(array("\n", "\r", "\t", " "), '', $firstLine);
+        $langCodes = explode( ";", $firstLine );
+
+        if( count($langCodes) < 2 )
+            $this->fatalError( "Syntax error in file '" . $this->name . "' Invalid header - expected lang pair" . PHP_EOL . "Found: " . $firstLine );
+
+        $this->setSrcLang( $langCodes[0] );
+        $this->setDestLang( $langCodes[1] );
+    }
+
     function __construct( $fileName ) {
         $this->name = $fileName;
         $this->handle = @fopen( $fileName, "r" );
         if( !$this->handle ) {
             $this->handle = null;
-            throw new RuntimeException( "File '$this->name' does not exist or can not be open" );
+            $this->fatalError( "File '$this->name' does not exist or can not be open" );
         }
 
-        echo "File '${fileName}' opened with handle " . $this->handle . PHP_EOL;
+        $this->processHeader();
     }
 
     function __destruct() {
         if( !is_null($this->handle) ) {
-            echo "Close file " . $this->name . " with handle " . $this->handle . PHP_EOL;
             fclose( $this->handle );
         }
     }
@@ -28,5 +57,29 @@ class TranslationFile {
         }
         return true;
     }
+
+    function getSrcLang()  {
+        return $this->srcLang;
+    }
+
+    function setSrcLang( $langCode ) {
+        $langCode = strtolower( $langCode );
+        if( !$this->isValidLangCode( $langCode ) )
+            $this->fatalError( "Source lang code '${langCode}' is Unknown or Unsuported" );
+
+        $this->srcLang = $langCode;
+    }
+
+    function getDestLang() {
+        return $this->destLang;
+    }
+
+    function setDestLang( $langCode ) {
+        $langCode = strtolower( $langCode );
+        if( !$this->isValidLangCode( $langCode ) )
+            $this->fatalError( "Destination lang code '${langCode}' is Unknown or Unsuported" );
+        $this->destLang = $langCode;
+    }
 }
+
 ?>
--- process_file.php
+++ process_file.php
@@ -5,8 +5,9 @@ require_once "lib/translationfile.php";
 
 try {
     $tfile = new TranslationFile( "translation_enpl.csv" );
+    echo "Translate " . $tfile->getSrcLang() . " => " . $tfile->getDestLang() . PHP_EOL;
 }
 catch( RuntimeException $e ) {
     echo "\n\nException : " . $e;
 }
-?>
+?> 
