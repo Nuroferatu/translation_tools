793c7cde1ef3de7587c32110a8a4414ed8ae2a6d
Nuroferatu <32645518+Nuroferatu@users.noreply.github.com>
2020-05-21 11:35:58 +0200

    printVar - more detailed debug

---
+++ lib/debugutils.php
@@ -0,0 +1,23 @@
+<?php
+
+function printVar( $var, $varName ) {
+    echo "DBG: '" . TColor::cyan($varName) . "' ";
+    echo "null[" . ( is_null($var) ? "T" : "F" ) . "] ";
+    echo "set[" . ( isset($var) ? "T" : "F" ) . "] ";
+    echo "empty[" . ( empty($var) ? "T" : "F" ) . "] ";
+    if( !is_null($var) ) {
+        if( is_object( $var ) || is_array( $var ) ) {
+            print_r( $var );
+        }
+        else {
+            echo "type[" . TColor::cyan( gettype($var) ) . "]=";
+            if( is_bool($var) )
+                echo Tcolor::green( ($var === true) ? "true" : "false" );
+            else    // TODO: Add rest of types
+                print_r( $var );
+        }
+    }
+    echo PHP_EOL;
+}
+
+?>
